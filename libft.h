254bfd657da6c4aeb9216bcdabb36431a67c1ec8
Banes <86240550+Banes-dev@users.noreply.github.com>
2023-10-19 13:06:52 +0200

    Add files via upload

--- libft.h
+++ libft.h
@@ -48,6 +48,7 @@ int			ft_toupper(char text);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
 char		*ft_strtrim(char *s1, char const *set);
+char		**ft_split(char *str, char *charset);
 
 // Bonus
 typedef struct s_list
