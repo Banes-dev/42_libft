4a27b81f6247d01faaf19eb070f31edb41d51736
Banes <86240550+Banes-dev@users.noreply.github.com>
2023-10-24 13:30:04 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -14,11 +14,9 @@ FUNCADDITIONAL =	ft_substr.c ft_strjoin.c ft_strtrim.c ft_split.c \
 					ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c \
 					ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
 
-BONUS =	ft_lstadd_back_bonus.c ft_lstadd_front_bonus.c ft_lstclear_bonus.c \
-		ft_lstdelone_bonus.c ft_lstiter_bonus.c ft_lstlast_bonus.c \
-		ft_lstmap_bonus.c ft_lstnew_bonus.c ft_lstsize_bonus.c
+BONUS =	ft_lstnew.c
 
-SRCSALL = ${FUNC} ${FUNCADDITIONAL}
+SRCSALL = ${FUNC} ${FUNCADDITIONAL} ${BONUS}
 OBJSALL = ${SRCSALL:.c=.o} 
 
 TARGET = exe
---
+++ ft_lstnew.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehay <ehay@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 13:18:02 by ehay              #+#    #+#             */
+/*   Updated: 2023/10/24 13:28:43 by ehay             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
